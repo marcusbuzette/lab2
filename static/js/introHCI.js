574765b2458abcb4b2f87bc00cfdc7af56d251f5
MarcusBuzette <marcusbuzete@me.com>
2018-01-18 16:15:48 -0800

    Added JavaScript

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -8,10 +8,19 @@ $(document).ready(function() {
 /*
  * Function that is called when the document is ready.
  */
+function projectClick(e) { 
+    // prevent the page from reloading      
+    e.preventDefault();
+    // In an event handler, $(this) refers to      
+    // the object that triggered the event      
+    $(this).css("background-color", "#7fff00");
+}
+ 
 function initializePage() {
 	$("#testjs").click(function(e) {
 		$('.jumbotron h1').text("Javascript is connected");
 	});
+	$("a.thumbnail").click(projectClick);
 
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
